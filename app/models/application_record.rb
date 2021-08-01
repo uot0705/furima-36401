bb6a6b235649440499865e87166176900c20b385
uot0705 <uot2345906abc@gmail.com>
2021-08-01 19:48:21 +0900

    ユーザー管理機能

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,7 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday,  presence: true
+  validates :first_name_kana, :last_name_kana, format: {with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name, :last_name,format: {with: /\A[ぁ-んァ-ン一-龥]/ }
 end
