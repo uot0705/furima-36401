aa5032ed5c2879daf286a3ba75d7222263cdb59e
uot0705 <uot2345906abc@gmail.com>
2021-08-02 17:04:45 +0900

    モデルの単体テストコード※修正３

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,17 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :first_name, :last_name, :first_name_kana, :last_name_kana, :birthday, presence: true
-  validates :first_name_kana, :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :first_name, :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ }
-  validates :encrypted_password,:password,format:{with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/}
-  
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, with: PASSWORD_REGEX
+with_options presence: true do
+  validates :nickname,:birthday
+  with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+    validates :first_name_kana, :last_name_kana
+  end
+  with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/ } do
+    validates :first_name, :last_name
+  end
+  with_options format: { with: /(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{6,}/ } do
+    validates :encrypted_password
+  end
 end
+PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
