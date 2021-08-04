ffa7871de5df8cb177d73e590dc8b78d51071de3
uot0705 <uot2345906abc@gmail.com>
2021-08-04 12:33:04 +0900

    修正版

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,12 +2,12 @@ FactoryBot.define do
   factory :item do
     name                    { 'テスト'}
     explanation            { 'テスト' }
-    product_category_id    { '2' }
-    product_condition_id   { '2' }
-    delivery_fee_id        { '2' }
-    prefecture_id          { '2' }
-    delivery_day_id        { '2' }
-    price                  { '1000' }
+    product_category_id    { 2 }
+    product_condition_id   { 2 }
+    delivery_fee_id        { 2 }
+    prefecture_id          { 2 }
+    delivery_day_id        { 2 }
+    price                  { 1000 }
     association :user
     
     after(:build) do |item|
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -72,7 +72,16 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include "Price is not a number"
       end
-      
+      it '価格は半角英数混合では登録できないこと' do
+        @item.price = "1234abcd"
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      end
+      it '価格は半角英語だけでは登録できないこと' do
+        @item.price = "aaaa"
+        @item.valid?
+        expect(@item.errors.full_messages).to include "Price is not a number"
+      end
     end
   end
 end
