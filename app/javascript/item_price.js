1188a838db91fa75356836dee816b4aac0d1df5c
uot0705 <uot2345906abc@gmail.com>
2021-08-04 11:39:20 +0900

    販売手数料実装

---
+++ app/javascript/item_price.js
@@ -0,0 +1,12 @@
+window.addEventListener('load', () => {
+
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+    const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = inputValue * 0.1
+    const profit = document.getElementById("profit");
+    profit.innerHTML = inputValue - addTaxDom.innerHTML
+
+ })
+});
