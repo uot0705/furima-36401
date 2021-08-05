361c84b9dc6f63fbcc76391fc5cdb12ff9226a3d
uot0705 <uot2345906abc@gmail.com>
2021-08-05 14:40:05 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :find_action ,only: [:show, :edit, :update, :move_to_index]
   before_action :move_to_index ,only: [:edit, :update]
-  before_action :find_action ,only: [:show, :edit, :update,]
+  
   def index
     @item = Item.all.order(id: "DESC")
     
@@ -34,20 +35,19 @@ def update
   end
 end
 
-def move_to_index
-  @item = Item.find(params[:id])
-  unless current_user == @item.user
-    redirect_to root_path
-  end
-end
 
-def find_action
-  @item = Item.find(params[:id])
-end
   private
   def item_params
     params.require(:item).permit(:name, :explanation, :product_category_id, :product_condition_id, 
       :delivery_fee_id, :prefecture_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
   end
-
+  def move_to_index
+    unless current_user == @item.user
+      redirect_to root_path
+    end
+  end
+  
+  def find_action
+    @item = Item.find(params[:id])
+  end
 end
