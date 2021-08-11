5c8f4c1e8de3e681903f9c02841a021dbb5a89c2
uot0705 <uot2345906abc@gmail.com>
2021-08-11 11:06:26 +0900

    共同確認修正１

--- app/models/item.rb
+++ app/models/item.rb
@@ -13,7 +13,7 @@ class Item < ApplicationRecord
 
   validates :product_category_id, :product_condition_id, :prefecture_id, :delivery_fee_id, 
   :delivery_day_id, numericality: { other_than: 1 , message: "can't be blank"} 
-  validates :price,format: { with: /\A[0-9]+\z/ },numericality: { only_integer: true,greater_than: 299, less_than: 9999999}
+  validates :price,format: { with: /\A[0-9]+\z/ },numericality: { only_integer: true,greater_than: 299, less_than: 10000000}
   validates :name, length: { maximum: 40 }
   validates :explanation, length: { maximum: 1000 }
 end
